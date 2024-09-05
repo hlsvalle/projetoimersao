b0c8f5a640a0be4bf8afc3abf68d3eac0a12946a
hlsvalle <hlsvalle@gmail.com>
2024-09-05 11:11:53 -0400

    Integração com o Javascript e a base de dados.

--- app.js
+++ app.js
@@ -1,2 +1,24 @@
-console.log(dados[0].descricao);
+function pesquisar() {
+    // Obtém a seção HTML onde os resultados da pesquisa serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+
+    // Inicializa uma string vazia para armazenar os resultados da pesquisa
+    let resultados = "";
+
+    // Itera sobre cada elemento do array 'dados'
+    for (let dado of dados) {
+        // Constrói o HTML para cada item do resultado da pesquisa
+        resultados += `<div class="item-resultado">
+            <h2>
+                <a href="#" target="_blank">${dado.titulo}</a>
+            </h2>
+            <p class="descricao-meta">${dado.descricao}</p>
+            <a href="${dado.link}" target="_blank">Mais informações</a>
+        </div>`;
+    }
+
+    // Atribui o HTML gerado para a seção de resultados
+    section.innerHTML = resultados;
+}
+
 
--- dados.js
+++ dados.js
@@ -1,13 +1,13 @@
 let dados = [
     {
         titulo: "Rayssa Leal",
-        descricao: "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma.Com apenas 16 anos, ela já é considerada uma das maiores skatistas de sua geração.",
+        descricao: "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Com apenas 16 anos, ela já é considerada uma das maiores skatistas de sua geração.",
         link: "https://pt.wikipedia.org/wiki/Rayssa_Leal"
     },
     {
         titulo: "Beatriz Souza",
         descricao: "Beatriz Souza é uma judoca brasileira que conquistou a medalha de ouro nas Olimpíadas de Tóquio. Sua história de superação e dedicação é inspiradora para muitos.",
-        link: "https://pt.wikipedia.org/wiki.org/Beatriz_Souza" // Ajustar o link se necessário
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza"
     },
     {
         titulo: "Rebeca Andrade",
