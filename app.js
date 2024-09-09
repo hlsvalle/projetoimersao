15cbdc68fc000cdb2906736c8b079b52587078a1
hlsvalle <hlsvalle@gmail.com>
2024-09-09 08:18:01 -0400

    Projeto Finalizado

--- app.js
+++ app.js
@@ -2,21 +2,46 @@ function pesquisar() {
     // Obtém a seção HTML onde os resultados da pesquisa serão exibidos
     let section = document.getElementById("resultados-pesquisa");
 
+    let campoPesquisa = document.getElementById("campo-pesquisa").value;
+
+    if (!campoPesquisa.trim()) {
+        section.innerHTML = "<h3>Nada foi encontrado!</h3>";
+        return;
+    }
+
+    //Converte todo o valor da Pesquisa em minusculo.
+    campoPesquisa = campoPesquisa.toLowerCase();
+
     // Inicializa uma string vazia para armazenar os resultados da pesquisa
     let resultados = "";
 
+    //Cria três strings vazias para armazenar o valor do titulo, descrição e tags em minusculo
+    let titulo = "";
+    let descricao = "";
+    let tags = ""
+
     // Itera sobre cada elemento do array 'dados'
     for (let dado of dados) {
-        // Constrói o HTML para cada item do resultado da pesquisa
-        resultados += `<div class="item-resultado">
-            <h2>
-                <a href="#" target="_blank">${dado.titulo}</a>
-            </h2>
-            <p class="descricao-meta">${dado.descricao}</p>
-            <a href="${dado.link}" target="_blank">Mais informações</a>
-        </div>`;
-    }
+        titulo = dado.titulo.toLowerCase();
+        descricao = dado.descricao.toLowerCase();
+        tags = dado.tags.toLowerCase();
 
+        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+            // Constrói o HTML para cada item do resultado da pesquisa
+            resultados += `<div class="item-resultado">
+                             <h2>
+                                <a href="#" target="_blank">${dado.titulo}</a>
+                             </h2>
+                             <p class="descricao-meta">${dado.descricao}</p>
+                             <a href="${dado.link}" target="_blank">Mais informações</a>
+                            </div>`;
+        }
+        
+    }
+    if (!resultados){
+        resultados = "<h3> Nada foi encontrado </h3>";
+    }
+    
     // Atribui o HTML gerado para a seção de resultados
     section.innerHTML = resultados;
 }
--- dados.js
+++ dados.js
@@ -2,17 +2,20 @@ let dados = [
     {
         titulo: "Rayssa Leal",
         descricao: "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Com apenas 16 anos, ela já é considerada uma das maiores skatistas de sua geração.",
-        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate sk8"
     },
     {
         titulo: "Beatriz Souza",
         descricao: "Beatriz Souza é uma judoca brasileira que conquistou a medalha de ouro nas Olimpíadas de Tóquio. Sua história de superação e dedicação é inspiradora para muitos.",
-        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza"
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "judo judô judoka"
     },
     {
         titulo: "Rebeca Andrade",
         descricao: "Rebeca Andrade é uma ginasta artística brasileira que conquistou o coração dos brasileiros com sua habilidade, determinação e carisma. Bicampeã olímpica, ela se tornou um símbolo do esporte nacional, inspirando jovens atletas e quebrando barreiras.",
-        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginástica ginastica ouro"
     }
 ];
 
